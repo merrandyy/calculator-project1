eae2c7d03aba714f38db4f2c7fdd5a9e4b00551d
Mariam andy <mariamalandejanii@gmail.com>
2024-07-14 19:07:45 +0300

    buttons using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,28 @@
     <title> calculator</title>
 </svelte:head>
 
-<div class=" h-[30rem] bg-white w-[20rem] rounded-3xl flex justify-center items-center"> 
-    <div class="bg-slate-400 h-[7rem] w-[7rem] flex-justify-center  rounded-full">
-
-    </div>
+<div class=" h-[30rem] bg-white w-[20rem] rounded-3xl grid grid-cols-4 gap-3 "> 
+    <button> % </button>
+    <button> √ </button>
+    <button> CE </button>
+    <button> C </button>
+    <button> 7 </button>
+    <button> 8 </button>
+    <button> 9 </button>
+    <button> - </button>
+    <button> 4 </button>
+    <button> 5 </button>
+    <button> 6 </button>
+    <button> ÷ </button>
+    <button> 1 </button>
+    <button> 2 </button>
+    <button> 3 </button>
+    <button> x </button>
+    <button> . </button>
+    <button> 0 </button>
+    <button> = </button>
+    <button> + </button>
 </div>
+
 
 
